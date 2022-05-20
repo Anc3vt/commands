e2523332737d4a4b6e8b095ef2335ad5af46b04f
Ancevt <i@ancevt.ru>
2022-05-20 18:43:20 +0300

    Fix license text

--- src/main/java/com/ancevt/util/command/Command.java
+++ src/main/java/com/ancevt/util/command/Command.java
@@ -1,6 +1,6 @@
 /*
- *   Ancevt Command
- *   Copyright (C) 2020 Ancevt (me@ancevt.com)
+ *   Ancevt Commands
+ *   Copyright (C) 2022 Ancevt (me@ancevt.com)
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
